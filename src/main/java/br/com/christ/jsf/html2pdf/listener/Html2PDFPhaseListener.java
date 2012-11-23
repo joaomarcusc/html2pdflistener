32f1f222b3fbe625f7206a34e310a3f779fd4a5d
joaomc <joaoma@gmail.com>
2012-11-23 07:28:20 -0200

    Arquivos iniciais do projeto

---
+++ src/main/java/br/com/christ/jsf/html2pdf/listener/Html2PDFPhaseListener.java
@@ -0,0 +1,102 @@
+package br.com.christ.jsf.html2pdf.listener;
+
+import org.w3c.dom.Document;
+import org.w3c.tidy.Tidy;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import javax.el.ELContext;
+import javax.el.MethodExpression;
+import javax.faces.application.Application;
+import javax.faces.application.ViewHandler;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+@SuppressWarnings("serial")
+public class Html2PDFPhaseListener implements PhaseListener {
+
+    public void afterPhase(PhaseEvent event) {
+    }
+
+    public void beforePhase(PhaseEvent event) {
+        HttpServletRequest request = ((HttpServletRequest)event.getFacesContext().getExternalContext().getRequest());
+        HttpServletResponse response = ((HttpServletResponse)event.getFacesContext().getExternalContext().getResponse());
+        if("1".equals(request.getAttribute("gerar_pdf")) && request.getAttribute("ja_gerou_pdf") == null) {
+            gerarPDF(event);
+        }
+    }
+
+    private void gerarPDF(PhaseEvent event) {
+        HttpServletRequest request = ((HttpServletRequest)event.getFacesContext().getExternalContext().getRequest());
+        HttpServletResponse response = ((HttpServletResponse)event.getFacesContext().getExternalContext().getResponse());
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        Object oldResponse = externalContext.getResponse();
+        String nomeArquivo = "cronograma.pdf";
+        try {
+            Application application = facesContext.getApplication();
+            ViewHandler viewHandler = application.getViewHandler();
+            ELContext elContext = facesContext.getELContext();
+            String actionPdf = null;
+            if(request.getAttribute("action_pdf") != null)
+                actionPdf = request.getAttribute("action_pdf").toString();
+            if(request.getAttribute("nome_arquivo_pdf") != null)
+                nomeArquivo = request.getAttribute("nome_arquivo_pdf").toString();
+            ResponseCatcher catcher = new ResponseCatcher(response);
+            externalContext.setResponse(catcher);
+            viewHandler.renderView(facesContext, facesContext.getViewRoot());
+            String htmlContent = catcher.toString();
+            externalContext.setResponse(oldResponse);
+            ByteArrayOutputStream pdfStream = new ByteArrayOutputStream();
+            Document xhtmlContent = null;
+            Tidy tidy = new Tidy();
+            try {
+                tidy.setFixUri(true);
+                tidy.setXHTML(true);
+                tidy.setShowWarnings(false);
+                tidy.setAsciiChars(true);
+                tidy.setNumEntities(true);
+                xhtmlContent = tidy.parseDOM(new ByteArrayInputStream(htmlContent.getBytes()),pdfStream);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                ITextRenderer renderer = new ITextRenderer();
+                renderer.setDocument(xhtmlContent, ((HttpServletRequest) request).getRequestURL().toString());
+                renderer.layout();
+                pdfStream.reset();
+                renderer.createPDF(pdfStream);
+
+            } catch (com.itextpdf.text.DocumentException e) {
+                throw new RuntimeException(e);
+            }
+
+            byte[] bytesPDF = pdfStream.toByteArray();
+            request.setAttribute("ja_gerou_pdf", "1");
+            if(actionPdf != null && !actionPdf.isEmpty()) {
+                response.setContentType("application/pdf");
+                response.addHeader("Content-Disposition","attachment; filename=cronograma.pdf");
+                response.getOutputStream().write(bytesPDF);
+                facesContext.responseComplete();
+            } else {
+                MethodExpression methodExpression = application.getExpressionFactory().createMethodExpression(elContext, actionPdf,
+                        String.class,
+                        new Class[]{byte[].class});
+                String outcome = methodExpression.invoke(elContext, new Object[] { bytesPDF }).toString();
+                facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, "", outcome);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public PhaseId getPhaseId() {
+        return PhaseId.RENDER_RESPONSE;
+    }
+}
---
+++ src/main/java/br/com/christ/jsf/html2pdf/listener/ResponseCatcher.java
@@ -0,0 +1,142 @@
+package br.com.christ.jsf.html2pdf.listener;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+public class ResponseCatcher implements HttpServletResponse {
+
+    CharArrayWriter output;
+
+    PrintWriter writer;
+
+    HttpServletResponse response;
+
+    public ResponseCatcher(HttpServletResponse response) {
+        this.response = response;
+        output = new CharArrayWriter();
+        writer = new PrintWriter(output, true);
+    }
+
+    public PrintWriter getWriter() {
+        return writer;
+    }
+
+    public void setCharacterEncoding(String charset) {
+        response.setCharacterEncoding(charset);
+    }
+
+    public void flushBuffer() throws IOException {
+        writer.flush();
+    }
+
+    public boolean isCommitted() {
+        return false;
+    }
+
+    public boolean containsHeader(String arg0) {
+        return false;
+    }
+
+    public String encodeURL(String arg0) {
+        return response.encodeURL(arg0);
+    }
+
+    public String encodeRedirectURL(String arg0) {
+        return response.encodeRedirectURL(arg0);
+    }
+
+    public String encodeUrl(String arg0) {
+        return response.encodeUrl(arg0);
+    }
+
+    public String encodeRedirectUrl(String arg0) {
+        return response.encodeRedirectUrl(arg0);
+    }
+
+    public String getCharacterEncoding() {
+        return response.getCharacterEncoding();
+    }
+
+    public String getContentType() {
+        return response.getContentType();
+    }
+
+    public int getBufferSize() {
+        return response.getBufferSize();
+    }
+
+    public Locale getLocale() {
+        return response.getLocale();
+    }
+
+    public void sendError(int arg0, String arg1) throws IOException {
+        response.sendError(arg0, arg1);
+    }
+
+    public void sendError(int arg0) throws IOException {
+        response.sendError(arg0);
+    }
+
+    public void sendRedirect(String arg0) throws IOException {
+        response.sendRedirect(arg0);
+    }
+
+    public void addCookie(Cookie arg0) {
+        response.addCookie(arg0);
+    }
+
+    public void setDateHeader(String arg0, long arg1) {
+        response.setDateHeader(arg0, arg1);
+    }
+
+    public void addDateHeader(String arg0, long arg1) {
+        response.addDateHeader(arg0, arg1);
+    }
+
+    public void setHeader(String arg0, String arg1) {
+        response.setHeader(arg0, arg1);
+    }
+
+    public void addHeader(String arg0, String arg1) {
+        response.addHeader(arg0, arg1);
+    }
+
+    public void setIntHeader(String arg0, int arg1) {
+        response.setIntHeader(arg0, arg1);
+    }
+
+    public void addIntHeader(String arg0, int arg1) {
+        response.addIntHeader(arg0, arg1);
+    }
+
+    public void setContentLength(int arg0) {
+        response.setContentLength(arg0);
+    }
+
+    public void setContentType(String arg0) {
+        response.setContentType(arg0);
+    }
+
+    /* null ops */
+    public void setStatus(int arg0) {}
+    public void setStatus(int arg0, String arg1) {}
+    public void setBufferSize(int arg0) {}
+    public void resetBuffer() {}
+    public void reset() {}
+    public void setLocale(Locale arg0) {}
+
+
+    public ServletOutputStream getOutputStream() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String toString() {
+        return output.toString();
+    }
+}
