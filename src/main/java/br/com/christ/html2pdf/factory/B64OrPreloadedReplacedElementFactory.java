7ad0dca892f6c3210bdcc44f72355a4cf654ab35
joaomc <joaoma@gmail.com>
2015-04-06 10:20:48 -0300

    Se uma imagem nao for encontrada, usar uma imagem padrao vazia (1x1 pixel)!

--- src/main/java/br/com/christ/html2pdf/factory/B64OrPreloadedReplacedElementFactory.java
+++ src/main/java/br/com/christ/html2pdf/factory/B64OrPreloadedReplacedElementFactory.java
@@ -1,6 +1,9 @@
 package br.com.christ.html2pdf.factory;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -20,10 +23,18 @@ import br.com.christ.html2pdf.loader.FacesResourceLoader;
 import br.com.christ.html2pdf.loader.ResourceLoader;
 import com.lowagie.text.BadElementException;
 import com.lowagie.text.Image;
+import com.lowagie.text.Jpeg;
 import com.lowagie.text.pdf.codec.Base64;
+import sun.misc.BASE64Decoder;
 
 
 public class B64OrPreloadedReplacedElementFactory implements ReplacedElementFactory {
+
+	private static final String EMPTY_IMAGE_CONTENT = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAAlwSFlzAAAL\n"+
+			"EwAACxMBAJqcGAAAAAd0SU1FB98EBg0LFXmmUzUAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRo\n"+
+			"IEdJTVBXgQ4XAAAADElEQVQI12P4//8/AAX+Av7czFnnAAAAAElFTkSuQmCC\n";
+
+	private Image emptyImage;
 
 	private LinkedHashMap<String, ReplacedElement> elementCache;
 
@@ -33,6 +44,12 @@ public class B64OrPreloadedReplacedElementFactory implements ReplacedElementFact
 		super();
 		setResourceLoader(new FacesResourceLoader());
 		elementCache = new LinkedHashMap<String, ReplacedElement>();
+		BASE64Decoder decoder = new BASE64Decoder();
+		try {
+			emptyImage = Image.getInstance(decoder.decodeBuffer(EMPTY_IMAGE_CONTENT));
+		} catch (Exception e) {
+			throw new RuntimeException("Erro durante a geração do PDF");
+		}
 	}
 
 	public B64OrPreloadedReplacedElementFactory(ResourceLoader resourceLoader) {
@@ -76,20 +93,24 @@ public class B64OrPreloadedReplacedElementFactory implements ReplacedElementFact
 		return imgElement;
 	}
 
-	protected FSImage buildImage(String srcAttr, UserAgentCallback uac) throws IOException, BadElementException {
+	protected FSImage buildImage(String srcAttr, UserAgentCallback uac) throws BadElementException, IOException {
 		FSImage fsImage;
-		if(srcAttr.startsWith("preload:")) {
-			String preloadSrc = srcAttr.substring("preload:".length());
-			fsImage = new ITextFSImage(Image.getInstance(getResourceLoader().getBytesFromReference(preloadSrc)));
-		} else if (srcAttr.startsWith("data:image/")) {
-			String b64encoded = srcAttr.substring(srcAttr.indexOf("base64,") + "base64,".length(), srcAttr.length());
-			byte[] decodedBytes = Base64.decode(b64encoded);
+		try {
+			if (srcAttr.startsWith("preload:")) {
+				String preloadSrc = srcAttr.substring("preload:".length());
+				fsImage = new ITextFSImage(Image.getInstance(getResourceLoader().getBytesFromReference(preloadSrc)));
+			} else if (srcAttr.startsWith("data:image/")) {
+				String b64encoded = srcAttr.substring(srcAttr.indexOf("base64,") + "base64,".length(), srcAttr.length());
+				byte[] decodedBytes = Base64.decode(b64encoded);
 
-			fsImage = new ITextFSImage(Image.getInstance(decodedBytes));
-		} else if (preloadAllImages) {
-			fsImage = new ITextFSImage(Image.getInstance(getResourceLoader().getBytesFromReference(srcAttr)));
-		} else {
-			fsImage = uac.getImageResource(srcAttr).getImage();
+				fsImage = new ITextFSImage(Image.getInstance(decodedBytes));
+			} else if (preloadAllImages) {
+				fsImage = new ITextFSImage(Image.getInstance(getResourceLoader().getBytesFromReference(srcAttr)));
+			} else {
+				fsImage = uac.getImageResource(srcAttr).getImage();
+			}
+		} catch(Exception exc) {
+			fsImage = new ITextFSImage(emptyImage);
 		}
 		return fsImage;
 	}
